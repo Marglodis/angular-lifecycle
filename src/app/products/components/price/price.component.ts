fa0abc9f8559fac68109eed26f9e347db95faadb
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-27 22:21:47 -0300

    ngOndestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy{
+
+  @Input() price: number = 0
+
+  public interval$?:Subscription;
+  
+  ngOnInit(): void {
+    console.log('Componente HIJO ngOnInit');
+
+    this.interval$ = interval(1000).subscribe(value => {
+      console.log('Tick', value);
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO ngOnChanges');
+  }
+  ngOnDestroy(): void {
+    console.log('Componente HIJO ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
 
 }
